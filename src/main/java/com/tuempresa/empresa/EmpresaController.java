f0a69749cff69116f841c8e1eb54df8e1c17851f
jdm <jguillen@advisoryservicesg.tech>
2025-07-11 04:19:00 -0600

    actualizacion de pagina web

--- src/main/java/com/tuempresa/empresa/EmpresaController.java
+++ src/main/java/com/tuempresa/empresa/EmpresaController.java
@@ -6,8 +6,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmpresaController {
 
-    @GetMapping("/")
-    public String inicio() {
-        return "index";
+    @GetMapping("/intro")
+    public String intro() {
+        return "intro";  // vista con video
     }
-}
+        @GetMapping("/")
+        public String inicio() {
+            return "index"; // index.html
+        }
+
+        @GetMapping("/cooperativa.html")
+        public String cooperativa() {
+            return "cooperativa.html"; // cooperativa.html
+        }
+
+        @GetMapping("/mision.html")
+        public String mision() {
+            return "mision"; // mision.html
+        }
+
+        @GetMapping("/vision.html")
+        public String vision() {
+            return "vision"; // vision.html
+        }
+
+        @GetMapping("/funcionamiento.html")
+        public String funcionamiento() {
+            return "funcionamiento"; // funcionamiento.html
+        }
+
+        @GetMapping("/proyectos.html")
+        public String proyectos() {
+            return "proyectos"; // proyectos.html
+        }
+
+        @GetMapping("/transformacion.html")
+        public String transformacion() {
+            return "transformacion"; // transformacion.html
+        }
+    }
+
+
